7913c238d9474c41c7614ef3a6faa604a864b510
Ivan Telychkin <Ivan.Telichkin@Edetek.com>
2018-05-22 21:39:26 +0300

    Prepare for deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'dist/departments', {index: false}));
+app.use(express.static(__dirname + '/dist/departments', {index: false}));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'departments', 'index.html'));
