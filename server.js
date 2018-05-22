24ce4fbf02b75cc30dc4367fa638121c1141f1a9
Ivan Telychkin <Ivan.Telichkin@Edetek.com>
2018-05-22 21:25:02 +0300

    Prepare for deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 
 
 // Serve only the static files form the dist directory
- app.use(express.static(__dirname + '/dist/departments/index.html'));
+app.use(express.static(__dirname, 'dist/departments', {index: false}));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/departments/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'departments', 'index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(PORT, function () {
